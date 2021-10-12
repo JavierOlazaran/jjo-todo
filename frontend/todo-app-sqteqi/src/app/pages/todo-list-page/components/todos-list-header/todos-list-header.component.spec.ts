b514565914fd66e349a0d69e1f0225a3be8292ab
Javier Olazaran <jolazaran@flexiti.com>
2021-10-11 21:46:25 -0300

    Fixed jest configs

--- frontend/todo-app-sqteqi/src/app/pages/todo-list-page/components/todos-list-header/todos-list-header.component.spec.ts
+++ frontend/todo-app-sqteqi/src/app/pages/todo-list-page/components/todos-list-header/todos-list-header.component.spec.ts
@@ -5,6 +5,9 @@ import { TodosListHeaderComponent } from './todos-list-header.component';
 describe('TodosListHeaderComponent', () => {
   let component: TodosListHeaderComponent;
   let fixture: ComponentFixture<TodosListHeaderComponent>;
+  let themeServiceMock = {
+    toggleTheme: jest.fn()
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -21,5 +24,17 @@ describe('TodosListHeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Toggle theme button', () => {
+
+    test('should trigger toggleTheme method', () => {
+      const toggleFNSpy = jest.spyOn(component, "toggleTheme");
+      const toggleButton = fixture.debugElement.nativeElement.querySelector('.theme__toggle__btn');
+      toggleButton.click();
+
+      expect(toggleFNSpy).toHaveBeenCalled();
+    })
+
   });
 });
