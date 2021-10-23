1e2523dd9c8bd0e4a76f44498675d8b543a87089
Javier Olazaran <jolazaran@flexiti.com>
2021-10-23 19:19:22 -0300

    Added test coverage

--- frontend/todo-app-sqteqi/src/app/core/services/tests/auth.service.spec.ts
+++ frontend/todo-app-sqteqi/src/app/core/services/tests/auth.service.spec.ts
@@ -26,4 +26,15 @@ describe('AuthService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('methods', () => {
+
+    test('should call http method correctly', () => {
+      service.login(credentialsMock);
+      expect(httpMock.post).toHaveBeenCalledWith(service["authEndpointUrl"]+'/login', credentialsMock);
+
+      service.register(credentialsMock);
+      expect(httpMock.post).toHaveBeenCalledWith(service["authEndpointUrl"]+'/register', credentialsMock);
+    });
+  });
 });
--- frontend/todo-app-sqteqi/src/app/pages/todo-list-page/components/todos-list-footer/todos-list-footer.component.spec.ts
+++ frontend/todo-app-sqteqi/src/app/pages/todo-list-page/components/todos-list-footer/todos-list-footer.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { TodosListFooterComponent } from './todos-list-footer.component';
 
@@ -21,5 +22,40 @@ describe('TodosListFooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Filter buttons', () => {
+
+    beforeEach(() => {
+      fixture.detectChanges();
+    });
+
+    test('should send the corresponding event', () => {
+      const filterAllButton = fixture.nativeElement.querySelector('#filter_all_btn');
+      const filterActiveButton = fixture.nativeElement.querySelector('#filter_active_btn');
+      const filterCompletedButton = fixture.nativeElement.querySelector('#filter_completed_btn');
+
+      const filterButtonEventSpy = jest.spyOn(component.filterButtonEvent, 'emit');
+
+      filterAllButton.click();
+      expect(filterButtonEventSpy).toHaveBeenCalledWith('FILTER_ALL');
+
+      filterActiveButton.click();
+      expect(filterButtonEventSpy).toHaveBeenCalledWith('FILTER_ACTIVE');
+
+      filterCompletedButton.click();
+      expect(filterButtonEventSpy).toHaveBeenCalledWith('FILTER_COMPLETED');
+    });
+  });
+
+  describe('Clear complete button', () => {
+
+    test('should emit clearCompleted', () => {
+      const filterActiveButton = fixture.nativeElement.querySelector('#clear_completed_btn');
+      const clearCompletedSpy = jest.spyOn(component.clearCompleted, 'emit');
+
+      filterActiveButton.click();
+      expect(clearCompletedSpy).toHaveBeenCalled();
+    });
   });
 });
